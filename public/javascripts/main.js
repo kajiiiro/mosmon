13ab3eec872f2a8fec543292d7a6c0153cd21ef6
kajiro <kajiro@isb.co.jp>
2016-10-12 00:15:07 +0900

    [add]wsやめてajaxで頑張る

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,19 @@
-$(function() {
-  var ws = null;
-  var open = function() {
-    if (ws === null) {
-      // ws = new WebSocket('ws://' + location.host + '/ws/echo', ['echo-protocol']);
-      ws = new WebSocket('ws://' + location.hostname + '/ws/echo', ['echo-protocol']);
-      ws.onopen = function(event) {
-        console.log(event);
-        var date = new Date.toISOString();
-        ws.send(date);
-        $('#main').prepend('<p>open!! ' + date + '</p>');
-      };
-      ws.onmessage = function(event) {
-        console.log(event);
-        $('#main').prepend('<p>message!!</p>');
-      };
-      ws.onerror = function(event) {
-        console.log(event);
-        $('#main').prepend('<p>error!!</p>');
-      };
-      ws.onclose = function(event) {
-        console.log(event);
-        $('#main').prepend('<p>close!!</p>');
-      };
+var monitor = function() {
+  $.ajax({
+    type    : 'GET',
+    url     : '/mqtt',
+    dataType: 'json',
+    success : function(json) {
+      json.forEach(function(item) {
+        $('#main').prepend('<p><span class="topic">'
+          + item.topic + '</span> : <span class="message">'
+          + item.message + '</span></p>');
+      });
     }
-  };
-  open();
+  });
+
+};
+
+$(function() {
+  setInterval(monitor, 3000);
 });
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'mosmon' });
 });
 
+var mqtt = require('mqtt');
+var options = {
+  clientId: 'mosmon',
+};
+var client = mqtt.connect('mqtt://localhost', options);
+client.on('connect', function() {
+  console.log('mqtt connect');
+  client.subscribe('$SYS/#', function(error) {
+    console.log('mqtt subscribe', error);
+  });
+});
+client.on('close', function() {
+  console.log('mqtt close');
+});
+client.on('error', function(error) {
+  console.error('mqtt error', error);
+});
+var messages = [];
+client.on('message', function(topic, message) {
+  if (Buffer.isBuffer(message)) {
+    message = message.toString();
+  }
+  messages.push({topic: topic, message: message});
+});
+
+router.get('/mqtt', function(req, res, next) {
+  var limit = Math.min(30, messages.length); // 一回で取得できる数を最大30に制限する
+  var _messages = messages.slice(0, 30);
+  for (var i = 0;i < limit;++i) {
+    message.shift();
+  }
+  res.json(_messages);
+});
+
 module.exports = router;
